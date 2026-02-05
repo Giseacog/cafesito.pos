cc16dd63dac21619ea7d9a4706c6309acd10e268
Gisela Aosta <gis@MacBook-Pro-de-Gisela.local>
2026-02-04 19:23:24 -0600

    Creation of register service

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRouter from "./routes/auth.routes";
+
+const app = express();
+
+app.use(express.json()); //Esta es para que express lea a al json
+dotenv.config();
+
+//Esto es por su seguridad, porque hay diferentes dominios
+const corsOptions = {
+  origin: ["http://localhost"],
+  methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
+  allowheaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use("/api/auth", authRouter);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ backend/src/lib/supabase.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+export const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+      detectSessionInUrl: false,
+    },
+  }
+);
