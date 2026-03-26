4adf25b68b92906e9483d3a0e0d8c269263a766b
Gisela Aosta <gis@MacBook-Pro-de-Gisela.local>
2026-03-25 21:06:34 -0600

    changes cart to card

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart',
   imports: [],
-  templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
 })
 export class CartComponent {
 
